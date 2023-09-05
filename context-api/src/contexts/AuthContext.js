855cab59c348807689b4a134a57c1f22d5c5abc9
Mugundh <mugundhjb@gmail.com>
2023-09-05 21:45:37 +0530

    Context with Functional Component

--- context-api/src/contexts/AuthContext.js
+++ context-api/src/contexts/AuthContext.js
@@ -1,24 +1,39 @@
-import React, {createContext} from 'react';
+import React, {createContext, useState} from 'react';
 
 export const AuthContext = createContext();
 
-class AuthContextProvider extends React.Component {
+// class AuthContextProvider extends React.Component {
 
-    state = {
-        isLoggedIn : false
-    }
+//     state = {
+//         isLoggedIn : false
+//     }
 
-    changeAuthStatus = () => {
-        this.setState({ isLoggedIn : !this.state.isLoggedIn });
+//     changeAuthStatus = () => {
+//         this.setState({ isLoggedIn : !this.state.isLoggedIn });
+//     };
+
+//     render(){
+//         return(
+//             <AuthContext.Provider value={{...this.state, changeAuthStatus: this.changeAuthStatus}}>
+//                 {this.props.children}
+//             </AuthContext.Provider>
+//         )
+//     }
+// }
+
+const AuthContextProvider = ({children}) => {
+    const [isLoggedIn, setIsloggedIn] = useState(false);
+
+    const changeAuthStatus = () => {
+        setIsloggedIn(!isLoggedIn);
     };
 
-    render(){
-        return(
-            <AuthContext.Provider value={{...this.state, changeAuthStatus: this.changeAuthStatus}}>
-                {this.props.children}
-            </AuthContext.Provider>
-        )
-    }
+    return(
+        <AuthContext.Provider value={{isLoggedIn, changeAuthStatus: changeAuthStatus}}>
+            {children}
+        </AuthContext.Provider>
+    )
+    
 }
 
 export default AuthContextProvider;
