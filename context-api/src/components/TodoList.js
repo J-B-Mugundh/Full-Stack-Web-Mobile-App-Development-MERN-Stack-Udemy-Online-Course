b909308b56cbbb2648f2ce02bfcee80483e1fc65
Mugundh <mugundhjb@gmail.com>
2023-09-05 20:05:25 +0530

    Context hooks

--- context-api/src/components/TodoList.js
+++ context-api/src/components/TodoList.js
@@ -1,22 +1,43 @@
-import React from 'react'
+// import React from 'react'
+// import { ThemeContext } from '../contexts/ThemeContext'
+
+
+// class TodoList extends React.Component {
+//   static contextType = ThemeContext; // We may or may not be able to use this context in functional component
+//   render() {
+//     const {isDarkTheme, darkTheme, lightTheme, changeTheme} = this.context;
+//         const theme = isDarkTheme ? darkTheme : lightTheme;
+//     return (
+//       <div style={{background: theme.background, color: theme.text, height: '140px', 
+//       textAlign: 'center' }}>
+//         <p className='item'>React js</p>
+//         <p className='item'>Anime</p>
+//         <p className='item'>Coding</p>
+//         <button className='ui button primary' onClick={changeTheme}>Change theme</button>
+//       </div>
+//     )
+//   }
+// }
+
+// export default TodoList
+
+// Using ContextHooks
+import React, {useContext} from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 
 
-class TodoList extends React.Component {
-  static contextType = ThemeContext
-  render() {
-    const {isDarkTheme, darkTheme, lightTheme, changeTheme} = this.context;
-        const theme = isDarkTheme ? darkTheme : lightTheme;
-    return (
-      <div style={{background: theme.background, color: theme.text, height: '140px', 
+const TodoList = () => {
+  const {isDarkTheme, darkTheme, lightTheme, changeTheme} = useContext(ThemeContext);
+  const theme = isDarkTheme ? darkTheme : lightTheme;
+  return(
+    <div style={{background: theme.background, color: theme.text, height: '140px', 
       textAlign: 'center' }}>
         <p className='item'>React js</p>
         <p className='item'>Anime</p>
         <p className='item'>Coding</p>
         <button className='ui button primary' onClick={changeTheme}>Change theme</button>
       </div>
-    )
-  }
+  )
 }
 
 export default TodoList
--- react-hooks/src/components/TodoList.js
+++ react-hooks/src/components/TodoList.js
@@ -1,18 +1,29 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import AddNewTodo from './AddNewTodo';
 import './TodoList.css';
 const TodoList = () => {
     // todos => actual value, setTodos => function to change the actual value
     const [todos, setTodos] = useState([
-        
+        {text: 'Task 1', id : 1}
     ]);
+
+    const [count, setCount] = useState(0);
 
     const addTodo = (text) => {
         setTodos([
-            ...todos, // this gives the earlier existing todos
+          //this gives the earlier existing todos
+            ...todos, // ... => spreader
             {text: text, id: Math.random()} // uuid package for unique id
         ])
     }
+
+    useEffect(() => { // Runs everytime the component renders
+        console.log('use effect', todos);
+    }, [todos]) // second parameter tells when to fire the useEffect (ie when which is modified)
+
+    useEffect(() => { // Runs everytime the component renders
+        console.log('use effect', count);
+    }, [count]) // second parameter tells when to fire the useEffect (ie when which is modified)
 
   return (
     <div className="TodoList">
@@ -25,6 +36,7 @@ const TodoList = () => {
         })}
       </ul>
       <AddNewTodo addTodo ={addTodo}/>
+      <button onClick={() => setCount(count + 1)}>Score: {count}</button>
     </div>
   )
 }
